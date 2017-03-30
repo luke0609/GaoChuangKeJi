4887525e49ed86af7f145a7b7d9e262dd0ec6e49
luke0609 <ljs19950609@163.com>
2017-03-30 10:45:44 +0800

    Merge branch 'master' of https://github.com/luke0609/GaoChuangKeJi
    Conflicts:
    	app/src/main/java/com/jiaokaokeji/gaochuangkeji/home/Activity/ProblemActivity.java

--- app/src/main/java/com/jiaokaokeji/gaochuangkeji/application/MyApplication.java
+++ app/src/main/java/com/jiaokaokeji/gaochuangkeji/application/MyApplication.java
@@ -8,6 +8,8 @@ import com.jiaokaokeji.gaochuangkeji.myclass.signview.ResolutionUtil;
 import org.xutils.BuildConfig;
 import org.xutils.x;
 
+import cn.smssdk.SMSSDK;
+
 
 /**
  * Created by Administrator on 2016/12/8.
@@ -19,6 +21,6 @@ public class MyApplication extends Application{
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG);
         ResolutionUtil.getInstance().init(this);
-
+        SMSSDK.initSDK(getApplicationContext(),"1c8cc63c2debc","e1ef2b7d9152a9c99609128c1c1127b7");//
     }
 }
--- app/src/main/java/com/jiaokaokeji/gaochuangkeji/login/SplashActivity.java
+++ app/src/main/java/com/jiaokaokeji/gaochuangkeji/login/SplashActivity.java
@@ -1,8 +1,12 @@
 package com.jiaokaokeji.gaochuangkeji.login;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
+import com.jiaokaokeji.gaochuangkeji.MainActivity;
 import com.jiaokaokeji.gaochuangkeji.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,6 +14,16 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        Handler x = new Handler();
+        x.postDelayed(new jumphandler(), 2000);
+
+    }
+    class jumphandler implements Runnable {
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
     }
 }
